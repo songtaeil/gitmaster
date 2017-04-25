6037fb35a92a6abd6ac55ee11493744fafadaa4c
tisong <songtaeil@gmail.com>
2017-04-25 11:41:56 +0900

    me/mycar-price added

--- src/main/java/myCar.java
+++ src/main/java/myCar.java
@@ -1,8 +1,17 @@
 
 public class myCar {
     private String carName;
+    private int price;
     
     public String getCarName(){
         return carName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
